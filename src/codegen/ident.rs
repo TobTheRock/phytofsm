8725b6e26915b2154f45ad2c013372b4764754f0
Tobias Waurick <tobtherock@proton.me>
2025-08-17 21:04:01 +0200

    chore: Move idents to seperate file

---
+++ src/codegen/ident.rs
@@ -0,0 +1,55 @@
+use heck::{ToSnakeCase, ToUpperCamelCase};
+use quote::format_ident;
+
+use crate::parser;
+
+pub struct Idents {
+    pub fsm: proc_macro2::Ident,
+    pub module: proc_macro2::Ident,
+    pub event_params_trait: proc_macro2::Ident,
+    pub event_enum: proc_macro2::Ident,
+    pub action_trait: proc_macro2::Ident,
+    pub state_struct: proc_macro2::Ident,
+}
+
+impl Idents {
+    pub fn new(name: &str) -> Idents {
+        let name = name.to_string();
+        Idents {
+            fsm: format_ident!("{}", name.to_upper_camel_case()),
+            module: format_ident!("{}", name.to_snake_case()),
+            event_params_trait: format_ident!("I{}EventParams", name.to_upper_camel_case()),
+            event_enum: format_ident!("{}Event", name.to_upper_camel_case()),
+            action_trait: format_ident!("I{}Actions", name.to_upper_camel_case()),
+            state_struct: format_ident!("{}State", name.to_upper_camel_case()),
+        }
+    }
+}
+
+impl parser::Event {
+    pub fn params_ident(&self) -> proc_macro2::Ident {
+        format_ident!("{}Params", self.0.to_upper_camel_case())
+    }
+
+    pub fn ident(&self) -> proc_macro2::Ident {
+        format_ident!("{}", self.0.to_upper_camel_case())
+    }
+}
+
+impl parser::Action {
+    pub fn ident(&self) -> proc_macro2::Ident {
+        format_ident!("{}", self.0.to_snake_case())
+    }
+}
+
+impl parser::State {
+    pub fn function_ident(&self) -> proc_macro2::Ident {
+        format_ident!("{}", self.name.to_snake_case())
+    }
+}
+
+impl quote::ToTokens for parser::State {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.name.to_tokens(tokens);
+    }
+}
--- src/fsm.rs
+++ src/fsm.rs
@@ -1,59 +1,10 @@
-use crate::{error, parser};
+use crate::{codegen::ident::Idents, error, parser};
 
 use heck::{ToSnakeCase, ToUpperCamelCase};
 use itertools::Itertools;
 use quote::format_ident;
 
 // TODO move to seperate file?
-pub struct Idents {
-    pub fsm: proc_macro2::Ident,
-    pub module: proc_macro2::Ident,
-    pub event_params_trait: proc_macro2::Ident,
-    pub event_enum: proc_macro2::Ident,
-    pub action_trait: proc_macro2::Ident,
-    pub state_struct: proc_macro2::Ident,
-}
-
-impl Idents {}
-pub fn new(name: &str) -> Idents {
-    let name = name.to_string();
-    Idents {
-        fsm: format_ident!("{}", name.to_upper_camel_case()),
-        module: format_ident!("{}", name.to_snake_case()),
-        event_params_trait: format_ident!("I{}EventParams", name.to_upper_camel_case()),
-        event_enum: format_ident!("{}Event", name.to_upper_camel_case()),
-        action_trait: format_ident!("I{}Actions", name.to_upper_camel_case()),
-        state_struct: format_ident!("{}State", name.to_upper_camel_case()),
-    }
-}
-
-impl parser::Event {
-    pub fn params_ident(&self) -> proc_macro2::Ident {
-        format_ident!("{}Params", self.0.to_upper_camel_case())
-    }
-
-    pub fn ident(&self) -> proc_macro2::Ident {
-        format_ident!("{}", self.0.to_upper_camel_case())
-    }
-}
-
-impl parser::Action {
-    pub fn ident(&self) -> proc_macro2::Ident {
-        format_ident!("{}", self.0.to_snake_case())
-    }
-}
-
-impl parser::State {
-    pub fn function_ident(&self) -> proc_macro2::Ident {
-        format_ident!("{}", self.name.to_snake_case())
-    }
-}
-
-impl quote::ToTokens for parser::State {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        self.name.to_tokens(tokens);
-    }
-}
 
 pub struct Fsm {
     repr: parser::ParsedFsm,
@@ -94,7 +45,7 @@ impl Fsm {
 impl TryFrom<parser::ParsedFsm> for Fsm {
     type Error = error::Error;
     fn try_from(repr: parser::ParsedFsm) -> Result<Self, Self::Error> {
-        let idents = new(repr.name());
+        let idents = Idents::new(repr.name());
         let entry = repr.enter_state().clone();
         Ok(Fsm {
             repr,
