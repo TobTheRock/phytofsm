41a64074e9cb7b381047e935c0eb6efd12469ac8
Tobias Waurick <tobtherock@proton.me>
2025-11-21 22:31:50 +0100

    test: IT for self transitions

--- tests/self_transition.rs
+++ tests/self_transition.rs
@@ -1,32 +1,43 @@
-// use phyto_fsm::generate_fsm;
-// generate_fsm!(
-//     file_path = "../src/test/self_transition/self_transition.puml",
-//     log_level = "debug"
-// );
+use phyto_fsm::generate_fsm;
+generate_fsm!(
+    file_path = "../src/test/self_transition/self_transition.puml",
+    log_level = "debug"
+);
 
-// use mockall::{mock, predicate};
-// use test_fsm::{ITestFsmActions, ITestFsmEventParams, TestFsm, TestFsmEvent};
-//
-// mock! {
-//     TestFsmActions {}
-//     impl ITestFsmActions for TestFsmActions {
-//         fn action1(&mut self, event: <MockTestFsmActions as ITestFsmEventParams>::SelfTransitionParams);
-//         fn action2(&mut self, event: <MockTestFsmActions as ITestFsmEventParams>::SelfTransitionParams);
-//     }
-// }
-//
-// impl ITestFsmEventParams for MockTestFsmActions {
-//     type SelfTransitionParams = ();
-//     type GoToBParams = ();
-// }
-//
-// #[test]
-// fn self_transition_action_called() {
-//     let mut actions = MockTestFsmActions::new();
-//     actions.expect_action1().returning(|_| ()).times(1);
-//     actions.expect_action2().returning(|_| ()).times(1);
-//     let mut fsm = TestFsm::new(actions);
-//     fsm.trigger_event(TestFsmEvent::SelfTransition(()));
-//     fsm.trigger_event(TestFsmEvent::SelfTransition(()));
-//     fsm.trigger_event(TestFsmEvent::GoToB(()));
-// }
+use mockall::mock;
+use test_fsm::{ITestFsmActions, ITestFsmEventParams, TestFsm, TestFsmEvent};
+
+mock! {
+    TestFsmActions {}
+    impl ITestFsmActions for TestFsmActions {
+        fn action1(&mut self, event: <MockTestFsmActions as ITestFsmEventParams>::SelfTransitionParams);
+        fn action2(&mut self, event: <MockTestFsmActions as ITestFsmEventParams>::SelfTransitionParams);
+    }
+}
+
+impl ITestFsmEventParams for MockTestFsmActions {
+    type SelfTransitionParams = ();
+    type GoToBParams = ();
+}
+
+#[test]
+fn self_transition_action_called() {
+    let mut actions = MockTestFsmActions::new();
+    actions.expect_action1().returning(|_| ()).times(1);
+    actions.expect_action2().returning(|_| ()).times(1);
+    let mut fsm = TestFsm::new(actions);
+
+    fsm.trigger_event(TestFsmEvent::SelfTransition(()));
+    fsm.trigger_event(TestFsmEvent::GoToB(()));
+}
+
+#[test]
+fn final_state() {
+    let mut actions = MockTestFsmActions::new();
+    actions.expect_action1().times(0);
+    actions.expect_action2().returning(|_| ()).times(1);
+    let mut fsm = TestFsm::new(actions);
+
+    fsm.trigger_event(TestFsmEvent::GoToB(()));
+    fsm.trigger_event(TestFsmEvent::SelfTransition(()));
+}
